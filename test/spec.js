ea9515cacc1c2a86655b671b417fc1d48bd4eaa8
Shahar Or <mightyiampresence@gmail.com>
2015-01-12 19:15:11 +0200

    Some changes

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,4 @@
-/* eslint-env node, browser, mocha */
+/* eslint-env browser, node, jasmine */
 "use strict";
 
 // Preparation
@@ -9,6 +9,7 @@ var discern;
 var sinon = require("sinon");
 var spy;
 var keyboard = require("keysim").Keyboard.US_ENGLISH;
+var emit = require("dom-events").emit;
 
 function press(key) {
     keyboard.dispatchEventsForAction(key, textarea);
@@ -19,46 +20,52 @@ function typeIn(keystrokes) {
     });
 }
 
-var expect = require("expect.js");
+var bun = require("expect.js");
 describe("TextEditDiscern", function(){
     it("throws when not given arguments", function(){
-        expect(function(){
+        bun(function(){
             discern = new TextEditDiscern();
         }).to.throwError();
     });
     it("throws when not given a DOM element", function(){
-        expect(function(){
+        bun(function(){
             discern = new TextEditDiscern("not a DOM element", function(){});
         }).to.throwError();
     });
     it("throws when not given a callback", function(){
-        expect(function(){
+        bun(function(){
             discern = new TextEditDiscern(textarea, 12412);
         }).to.throwError();
     });
     it("instantiates on an element", function(){
         spy = sinon.spy();
         discern = new TextEditDiscern(textarea, spy);
-        expect(discern).to.be.a(TextEditDiscern);
+        bun(discern).to.be.a(TextEditDiscern);
     });
     it("does not callback as long as text is added", function(){
         typeIn("The quick, brown fox jumped over the lazy dog.");
-        expect(spy.callCount).to.be(0);
+        bun(spy.callCount).to.be(0);
     });
     it("calls back if text is removed after text is added", function(){
         press("backspace");
-        expect(spy.callCount).to.be(1);
+        bun(spy.callCount).to.be(1);
     });
     it("calls back if text is added after text is removed", function(){
         typeIn("Foooooooooooooooo");
-        expect(spy.callCount).to.be(2);
+        bun(spy.callCount).to.be(2);
     });
     it("calls back if text is removed forward after text is added", function(){
         press("delete");
-        expect(spy.callCount).to.be(3);
+        bun(spy.callCount).to.be(3);
     });
     it("calls back if text is added after text is removed forward", function(){
         typeIn("Barr.");
-        expect(spy.callCount).to.be(4);
+        bun(spy.callCount).to.be(4);
+    });
+    it("calls back on paste after text removed", function(){
+        emit(textarea, "paste");
+        emit(textarea, "paste");
+        emit(textarea, "paste");
+        bun(spy.callCount).to.be(5);
     });
 });
