440d680177e04157382ea45e77714f6b88651d7c
Shahar Or <mightyiampresence@gmail.com>
2015-02-04 18:29:09 +0200

    Add standard and fix code styles

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Mon Jan 12 2015 17:57:20 GMT+0200 (IST)
 
-var specFile = "test/spec.js";
+var specFile = 'test/spec.js'
 
 module.exports = function(config) {
   var confObject = {
@@ -9,69 +9,57 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [
-        "jasmine",
-        "browserify"
+      'jasmine',
+      'browserify'
     ],
-
 
     // list of files / patterns to load in the browser
     files: [specFile],
 
-
     browserify: {
-        debug: true
+      debug: true
     },
-
 
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["progress"],
-
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [],
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  };
+  }
 
-  confObject.preprocessors[specFile] = ["browserify"];
+  confObject.preprocessors[specFile] = ['browserify']
 
-  config.set(confObject);
-};
+  config.set(confObject)
+}
