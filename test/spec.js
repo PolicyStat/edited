ca552affdefd934a345c1542ccd68b9aa0e2c69a
Shahar Or <mightyiampresence@gmail.com>
2015-02-05 14:10:17 +0200

    Love

--- test/spec.js
+++ test/spec.js
@@ -1,126 +1,140 @@
 /* eslint-env browser, node, jasmine */
-"use strict";
+'use strict'
 
 // Preparation
-var textarea = document.createElement("textarea");
-document.body.appendChild(textarea);
-var TextEditDiscern = require("..");
-var discern;
-var spy = jasmine.createSpy();
-var keyboard = require("keysim").Keyboard.US_ENGLISH;
-var emit = require("dom-events").emit;
-var randomChars = require("randomatic");
-var callNTimes = require("call-n-times");
-var forEach = require("foreach");
-var rnd = require("rnd");
+var TextEditDiscern = require('..')
+var discern
+var keyboard = require('keysim').Keyboard.US_ENGLISH
+var emit = require('dom-events').emit
+var randomChars = require('randomatic')
+var forEach = require('foreach')
+
+var editable = document.createElement('div')
+document.body.appendChild(editable)
+editable.contentEditable = true
+editable.style.height = '10em'
+editable.style.width = '20em'
+editable.style.border = '1px solid pink'
 
 var press = function (key) {
-    keyboard.dispatchEventsForAction(key, textarea);
-};
+  keyboard.dispatchEventsForAction(key, editable)
+}
 
 var editTypes = {
-    characterAddition: {
-        eachEventTriggersCallback: false,
-        triggerFunc: function() {
-            press(randomChars("*", 1));
-        }
-    },
-    backwardsRemoval: {
-        eachEventTriggersCallback: false,
-        triggerFunc: function() {
-            press("backspace", 10);
-        }
-    },
-    forwardRemoval: {
-        eachEventTriggersCallback: false,
-        triggerFunc: function () {
-            press("delete", 10);
-        }
-    },
-    space: {
-        eachEventTriggersCallback: false,
-        triggerFunc: function () {
-            press(" ");
-        }
-    },
-    paste: {
-        eachEventTriggersCallback: true,
-        triggerFunc: function () {
-            emit(textarea, "paste");
-        }
+  characterAddition: {
+    eachEventTriggersCallback: false,
+    triggerFunc: function() {
+      press(randomChars('*', 1))
     }
-};
+  },
+  backwardsRemoval: {
+    eachEventTriggersCallback: false,
+    triggerFunc: function() {
+      press('backspace', 10)
+    }
+  },
+  forwardRemoval: {
+    eachEventTriggersCallback: false,
+    triggerFunc: function () {
+      press('delete', 10)
+    }
+  },
+  space: {
+    eachEventTriggersCallback: false,
+    triggerFunc: function () {
+      press(' ')
+    }
+  },
+  paste: {
+    eachEventTriggersCallback: true,
+    triggerFunc: function () {
+      emit(editable, 'paste')
+    }
+  },
+  drop: {
+    eachEventTriggersCallback: true,
+    triggerFunc: function () {
+      emit(editable, 'drop')
+    }
+  }
+}
 
 var triggerEditType = function(editType) {
-    var times = editType.eachEventTriggersCallback ? 1 : rnd(999, 2);
+  editType.triggerFunc()
+}
 
-    callNTimes(editType.triggerFunc, times);
-};
+describe('Instantiation', function() {
+  it('throws when not given arguments', function() {
+    expect(function() {
+      discern = new TextEditDiscern()
+    }).toThrow()
+  })
+  it('throws when not given a DOM element', function() {
+    expect(function() {
+      discern = new TextEditDiscern('not a DOM element', function() {})
+    }).toThrow()
+  })
+  it('throws when not given a callback', function() {
+    expect(function() {
+      discern = new TextEditDiscern(editable, 12412)
+    }).toThrow()
+  })
+  it('instantiates on an element', function() {
+    var spy = jasmine.createSpy()
+    discern = new TextEditDiscern(editable, spy)
+    expect(discern instanceof TextEditDiscern).toBe(true)
+  })
+})
 
-describe("TextEditDiscern", function(){
-    it("throws when not given arguments", function(){
-        expect(function(){
-            discern = new TextEditDiscern();
-        }).toThrow();
-    });
-    it("throws when not given a DOM element", function(){
-        expect(function(){
-            discern = new TextEditDiscern("not a DOM element", function(){});
-        }).toThrow();
-    });
-    it("throws when not given a callback", function(){
-        expect(function(){
-            discern = new TextEditDiscern(textarea, 12412);
-        }).toThrow();
-    });
-    it("instantiates on an element", function(){
-        discern = new TextEditDiscern(textarea, spy);
-        expect(discern instanceof TextEditDiscern).toBe(true);
-    });
-});
+describe('testing', function() {
+  var spy = jasmine.createSpy()
+  var discern = new TextEditDiscern(editable, spy)
+  // Loop over the edit types
+  forEach(editTypes, function(thisEditType, thisEditTypeName) {
+    describe('(F)ormer = ' + thisEditTypeName + ',', function() {
+      thisEditType.name = thisEditTypeName
+      var otherEditTypes = {}
 
-describe("an instance", function() {
-    it("calls back wisely", function(){
-        // Loop over the edit types
-        forEach(editTypes, function(thisEditType){
-            var otherEditTypes = [];
+      // Get a list of all the other edit types (not current one)
+      forEach(editTypes, function(editType, name) {
+        if (editType !== thisEditType) {
+          otherEditTypes[name] = editType
+        }
+      })
 
-            // Get a list of all the other edit types (not current one)
-            forEach(editTypes, function(editType){
-                if (editType !== thisEditType) {
-                    otherEditTypes.push(editType);
-                }
-            });
+      // Go over the other edit types
+      forEach(otherEditTypes, function(otherEditType, otherEditTypeName) {
+        describe('(L)atter = ' + otherEditTypeName, function() {
+          it('does not call back, initially', function() {
+            expect(spy).not.toHaveBeenCalled()
+          })
 
-            // Go over the other edit types
-            otherEditTypes.forEach(function(otherEditType){
-                // CB not yet called
-                expect(spy).not.toHaveBeenCalled();
+          it('calls back on F', function() {
+            triggerEditType(thisEditType)
+            expect(spy.calls.count()).toEqual(1)
+          })
 
-                // First edit type always triggers callback
-                triggerEditType(thisEditType);
-                expect(spy.calls.count()).toEqual(1);
+          it('calls back on F,L', function() {
+            triggerEditType(otherEditType)
+            expect(spy.calls.count()).toEqual(2)
+          })
 
-                // Triggering a different edit type triggers callback
-                triggerEditType(otherEditType);
-                expect(spy.calls.count()).toEqual(2);
+          it('calls back F,L,F', function() {
+            triggerEditType(thisEditType)
+            expect(spy.calls.count()).toEqual(3)
+          })
 
-                // Triggering the previous edit type again triggers callback
-                triggerEditType(thisEditType);
-                expect(spy.calls.count()).toEqual(3);
+          it(thisEditType.eachEventTriggersCallback ? 'does' : 'does not' +
+            ' call back on F,L,F,F', function() {
+            triggerEditType(thisEditType)
+            expect(spy.calls.count())
+              .toEqual(thisEditType.eachEventTriggersCallback ? 4 : 3)
 
-                if (thisEditType.eachEventTriggersCallback) {
-                    // This type of edit is triggered only once by
-                    // `triggerEditType` and we would like to see that another
-                    // triggering of it triggers a callback.
-                    triggerEditType(thisEditType);
-                    expect(spy.calls.count()).toEqual(4);
-                }
-
-
-                discern.reset();
-                spy.calls.reset();
-            });
-        });
-    });
-});
+            discern.reset()
+            spy.calls.reset()
+          })
+        })
+      })
+    })
+  })
+})
