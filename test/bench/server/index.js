095448c9cecf2ed9cba89dae75fbaeac5906116f
Shahar Or <mightyiampresence@gmail.com>
2015-05-01 11:56:08 +0300

    testbench server: some code doc

--- test/bench/server/index.js
+++ test/bench/server/index.js
@@ -2,4 +2,15 @@ var server = require('./server')
 var getPort = require('./get-port')
 var listen = require('./listen')
 
+/*
+ * A promise that fulfills to the state object
+ * of the running testbench server.
+ *
+ * Properties are:
+ *  * `server` the express Application instance
+ *  * `port` the port on which it is listening
+ *
+ *  This module can be required or run standalone
+ *  (`node test/bench/server`).
+ */
 module.exports = getPort(server).then(listen)
