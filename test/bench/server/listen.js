459483a68f49155ae35d104882825cc83166b249
Shahar Or <mightyiampresence@gmail.com>
2015-05-15 15:24:21 +0300

    Semantic change in testbench server state properties

--- test/bench/server/listen.js
+++ test/bench/server/listen.js
@@ -1,6 +1,6 @@
 module.exports = function (state) {
   return new Promise(function (fulfill) {
-    state.server.listen(state.port, function (err) {
+    state.server = state.app.listen(state.port, function (err) {
       if (err) {
         throw err
       } else {
--- test/bench/server/setup.js
+++ test/bench/server/setup.js
@@ -1,13 +1,13 @@
 var Promise = require('promise')
 var browserify = require('browserify-middleware')
-var server = require('express')()
+var app = require('express')()
 
-server.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.send(require('./html'))
 })
-server.get('/index.js', browserify('./test/bench/client/index.js'))
+app.get('/index.js', browserify('./test/bench/client/index.js'))
 
 module.exports = new Promise(function (fulfill) {
-  var state = {server: server}
+  var state = {app: app}
   fulfill(state)
 })
