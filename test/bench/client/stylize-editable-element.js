6044660491ffd597964ec65928618782cabab9dd
Shahar Or <mightyiampresence@gmail.com>
2015-05-16 09:28:51 +0300

    testbench style changes to editable elements

--- test/bench/client/stylize-editable-element.js
+++ test/bench/client/stylize-editable-element.js
@@ -1,6 +1,8 @@
 module.exports = function ($editableElement) {
   $editableElement.css({
-    'min-height': '1em',
+    'min-height': '1.618em',
+    'min-width': '16.18em',
+    'display': 'block',
     border: '2px dotted pink'
   })
 }
