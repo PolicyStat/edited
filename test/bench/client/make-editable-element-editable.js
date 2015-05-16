7823f3396c1f4d3c9469f40585b50d9c3dc0ab07
Shahar Or <mightyiampresence@gmail.com>
2015-05-16 09:29:40 +0300

    testbench: do not mark contentEditable on already editables

--- test/bench/client/make-editable-element-editable.js
+++ test/bench/client/make-editable-element-editable.js
@@ -1,5 +1,3 @@
 module.exports = function ($editableElement) {
-  if (!$editableElement.is('textarea')) {
-    $editableElement.attr('contentEditable', true)
-  }
+  $editableElement.attr('contentEditable', true)
 }
--- test/bench/client/make-editable-element-in-editables-table.js
+++ test/bench/client/make-editable-element-in-editables-table.js
@@ -1,11 +1,14 @@
 var $ = require('jquery')
 var stylizeEditableElement = require('./stylize-editable-element')
 var makeEditableElementEditable = require('./make-editable-element-editable')
+var alreadyEditableElementsTags = require('./already-editable-elements-tags')
 
 module.exports = function ($editablesTable, tag) {
   var $editableElement = $('<' + tag + ' class="editable-element">')
   $editablesTable.find('tr.' + tag + ' td.element')
     .append($editableElement)
   stylizeEditableElement($editableElement)
-  makeEditableElementEditable($editableElement)
+  if (alreadyEditableElementsTags.indexOf(tag) === -1) {
+    makeEditableElementEditable($editableElement)
+  }
 }
