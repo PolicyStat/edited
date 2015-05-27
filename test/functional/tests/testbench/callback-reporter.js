82ede473df4893c94a02f759f2fb29830b8b089d
Shahar Or <mightyiampresence@gmail.com>
2015-05-27 15:42:40 +0300

    Fix a functional test: Callback reporter

--- test/functional/tests/testbench/callback-reporter.js
+++ test/functional/tests/testbench/callback-reporter.js
@@ -3,11 +3,12 @@ this['Callback reporter'] = function (browser) {
 
   var increaser = function () {
     browser.page.testbench()
-      .typeInEditableElement('. must increase counters!')
+      .typeInEditableElement('—increase counters')
   }
 
   browser.page.testbench()
     .forEachTestElementTagName(function (testElementTagName) {
+      browser.page.testbench().selectTestElementTagName(testElementTagName)
       browser.page.testbench().assertCallbackCountersAreVisible()
 
       browser.page.testbench().forEachCallbackType(function (callbackType) {
